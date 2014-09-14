1cd8d6f80503db5456e998289dc697d8194978a4
Jace Ju <jaceju@gmail.com>
2014-09-15 01:22:13 +0800

    加入 Data Provider

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -5,34 +5,30 @@ require __DIR__ . '/../Cart.php';
 
 class CartTest extends PHPUnit_Framework_TestCase
 {
-    public function testUpdateQuantitiesAndGetTotal()
+    /**
+     * @dataProvider provider
+     */
+    public function testUpdateQuantitiesAndGetTotal($quantities, $expected)
     {
         $cart = new Cart();
 
-        // Test 1
-        $quantities = [
-            1, 0, 0, 0, 0, 0,
-        ];
         $cart->updateQuantities($quantities);
-        $this->assertEquals(199, $cart->getTotal());
-
-        // Test 2
-        $quantities = [
-            1, 0, 0, 2, 0, 0,
-        ];
-        $cart->updateQuantities($quantities);
-        $this->assertEquals(797, $cart->getTotal());
-
-        return $cart;
+        $this->assertEquals($expected, $cart->getTotal());
     }
 
-    /**
-     * @depends testUpdateQuantitiesAndGetTotal
-     */
-    public function testGetProducts($cart)
+    public function provider()
     {
+        return [
+            [ [ 1, 0, 0, 0, 0, 0 ], 199 ],
+            [ [ 1, 0, 0, 2, 0, 0 ], 797 ],
+        ];
+    }
+
+    public function testGetProducts()
+    {
+        $cart = new Cart();
         $products = $cart->getProducts();
         $this->assertEquals(6, count($products));
-        $this->assertEquals(2, $products[3]['quantity']);
+        $this->assertEquals(0, $products[3]['quantity']);
     }
 }
