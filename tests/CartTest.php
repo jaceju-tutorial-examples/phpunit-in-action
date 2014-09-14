fc61c87611e3f7c8c500addb4538252325707449
Jace Ju <jaceju@gmail.com>
2014-09-14 23:44:30 +0800

    修正測試程式

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -1,28 +1,31 @@
 <?php
 /* tests/CartTest.php */
 
-require __DIR__ . '/../cart.php';
+require __DIR__ . '/../Cart.php';
 
 class CartTest extends PHPUnit_Framework_TestCase
 {
     public function testCart()
     {
-        $products = $GLOBALS['products'];
+        $cart = new Cart();
 
         // Test 1
         $quantities = [
             1, 0, 0, 0, 0, 0,
         ];
-        $products = updateQuantities($quantities, $products);
-        $total = updateTotal($products);
-        $this->assertEquals(199, $total);
+        $cart->updateQuantities($quantities);
+        $this->assertEquals(199, $cart->getTotal());
 
         // Test 2
         $quantities = [
             1, 0, 0, 2, 0, 0,
         ];
-        $products = updateQuantities($quantities, $products);
-        $total = updateTotal($products);
-        $this->assertEquals(797, $total);
+        $cart->updateQuantities($quantities);
+        $this->assertEquals(797, $cart->getTotal());
+
+        // Test 3
+        $products = $cart->getProducts();
+        $this->assertEquals(6, count($products));
+        $this->assertEquals(2, $products[3]['quantity']);
     }
 }
