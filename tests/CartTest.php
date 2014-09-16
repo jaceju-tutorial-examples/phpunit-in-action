94b1d9fe2620c480d0826e6855ccffb243a9c330
Jace Ju <jaceju@gmail.com>
2014-09-16 10:07:35 +0800

    加入 Fixture

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -5,15 +5,26 @@ require __DIR__ . '/../Cart.php';
 
 class CartTest extends PHPUnit_Framework_TestCase
 {
+    private $cart = null;
+
+    public function setUp()
+    {
+        $this->cart = new Cart();
+    }
+
+    public function tearDown()
+    {
+        $this->cart = null;
+    }
+
     /**
      * @dataProvider provider
      * @group update
      */
     public function testUpdateQuantitiesAndGetTotal($quantities, $expected)
     {
-        $cart = new Cart();
-        $cart->updateQuantities($quantities);
-        $this->assertEquals($expected, $cart->getTotal());
+        $this->cart->updateQuantities($quantities);
+        $this->assertEquals($expected, $this->cart->getTotal());
     }
 
     public function provider()
@@ -31,9 +42,8 @@ class CartTest extends PHPUnit_Framework_TestCase
     public function testUpdateQuantitiesWithException()
     {
         $this->setExpectedException('CartException');
-        $cart = new Cart();
         $quantities = [ -1, 0, 0, 0, 0, 0 ];
-        $cart->updateQuantities($quantities); // 預期會產生一個 Exception
+        $this->cart->updateQuantities($quantities); // 預期會產生一個 Exception
     }
 
     /**
@@ -41,8 +51,7 @@ class CartTest extends PHPUnit_Framework_TestCase
      */
     public function testGetProducts()
     {
-        $cart = new Cart();
-        $products = $cart->getProducts();
+        $products = $this->cart->getProducts();
         $this->assertEquals(6, count($products));
         $this->assertEquals(0, $products[3]['quantity']);
     }
