3f203cfe3a22e5ebbab07fc553b314baff5eb65c
Jace Ju <jaceju@gmail.com>
2014-09-18 23:51:43 +0800

    加入商品類別骨架

--- Product.php
+++ Product.php
@@ -2,5 +2,19 @@
 
 class Product
 {
+    public function __construct($name, $price, $tag)
+    {
+    }
 
+    public function getName()
+    {
+    }
+
+    public function getPrice()
+    {
+    }
+
+    public function getTag()
+    {
+    }
 }
