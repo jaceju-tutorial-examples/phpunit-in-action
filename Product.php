a53f0bcfc13888f6b5640e495c36576a8e03d5d2
Jace Ju <jaceju@gmail.com>
2014-09-19 00:25:11 +0800

    第二個 TDD 循環

--- Product.php
+++ Product.php
@@ -2,19 +2,27 @@
 
 class Product
 {
+    private $name = '', $price = 0, $tag = '';
+
     public function __construct($name, $price, $tag)
     {
+        $this->name = $name;
+        $this->price = $price;
+        $this->tag = $tag;
     }
 
     public function getName()
     {
+        return $this->name;
     }
 
     public function getPrice()
     {
+        return $this->price;
     }
 
     public function getTag()
     {
+        return $this->tag;
     }
 }
--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -4,4 +4,11 @@ require_once __DIR__ . '/../Product.php';
 
 class ProductTest extends PHPUnit_Framework_TestCase
 {
+    public function testProductConstructorAndGetter()
+    {
+        $product = new Product('商品1', 245, 'R');
+        $this->assertEquals('商品1', $product->getName());
+        $this->assertEquals(245, $product->getPrice());
+        $this->assertEquals('R', $product->getTag());
+    }
 }
